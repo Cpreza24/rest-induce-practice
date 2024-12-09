54c4fe8721556678cdeae03accfd173d679b2528
Cesar Preza <cpreza93@yahoo.com>
2024-12-08 19:57:31 -0800

    refactored paths

--- server.js
+++ server.js
@@ -1,119 +1,49 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require('method-override');
-const books = require('./data/books');
+const methodOverride = require('method-override')
 const app = express();
 const path = require('path');
-const { isBuffer } = require('util');
-const Book = require('./models/books');
-const mongoose = require('mongoose');
+
+
 // ==================
 // CONFIGURE MONGOOSE
 // ==================
-// getting-started.js
 require('./configs/database');
 
-// ************
-//  MIDDLEWARE
-// *************
+
+// ********************
+//    MIDDLEWARE
+// ********************
 
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', require('./routes/home'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(methodOverride('_method'))
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set('views', './views'); 
+app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')))
 
+// ***************************
+//      ROUTES (I.N.D.U.C.E)
+// ***************************
 
-// ROUTES (I.N.D.U.C.E.S) Follow this method when making routes!!
-//seed route
-app.use('/', require('./routes/seed'))
+// Seed Route
+app.use('/', require('./routes/seed'));
 
-// INDEX (Root Route)
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// Home Route
+app.use('/', require('./routes/home'));
 
-// INDEX
-app.get('/books', (req, res) => {
-    res.render('books', {title: 'Book List', books});
-});
+// Book Routes
+app.use('/', require('./routes/book'));
 
-
-// NEW
-app.get('/books/new', (req, res) => {
-    //res.json({message: 'Form with an empty form to submit the new book'});
-    res.render('books/new', { title: 'New Book' })
-});
-
-
-// CREATE
-app.post('/books', (req, res) => {
-    const newBook = {
-        id: books.length + 1,
-        title: req.body.title || 'New Book',
-        author: req.body.author || 'Cesar Preza',
-    }
-
-    books.push(newBook);
-    console.log(books);
-    res.status(201).redirect('/books');
-})
-
-// SHOW
-app.get('/books/:id', (req, res) => {
-        //NEVER use loops inside API
-        // When passing a variable into a URL, Must convert the string variable into a number.
-    const book = books.find(book => book.id === parseInt(req.params.id));
-    if (book) {
-        res.render('books/show', {title: 'Book Details', book})
-    } else {
-        res.status(404).render('404/notFound', {title: 'Book not found'});
-    }
-});
-
-app.get('/books/:id/edit', (req, res) => {
-    const book = books.find(book => book.id === parseInt(req.params.id));
-    if (book) {
-        res.render('books/edit', { title: 'Edit Book', book });
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found!' })
-    }
-})
-
-// EDIT 
-app.put('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIndex = books.findIndex(book => book.id === bookId);
-    if (bookIndex !== -1) {
-        books[bookIndex] = {...books[bookIndex], ...req.body};
-        //res.json({message: 'Book updated successfuflly', book: books[bookIndex]});
-        res.status(200).redirect('/books')
-    } else {
-        res.status(404).render('404/notFound', {title: 'Book Not Found'});
-    };
-});
-
-
-// DELETE 
-app.delete('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIdx = books.findIndex(book => book.id === bookId);
-    if (bookIdx !== -1) {
-        books.splice(bookIdx, 1);
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-    res.status(200).redirect('/books');
-})
-
-
+// ********************
+//    LISTENNER
+// ********************
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
+    console.log(`🎧 Server is running on http://localhost:${PORT}`);
 })
