6195af7d615d2aac473cf22d0de2a47ceb692bd2
Cesar Preza <cpreza93@yahoo.com>
2024-12-18 19:51:23 -0800

    feat: adds user sign up validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
 
 router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs');
+});
+
+//Validation
+router.post('/sign-up', async (req, res) => {
+    // Checks to see if the username is available
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send('username taken');
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and Confirm password must match');
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    const user = await User.create(req.body);
+    res.send(`thanks for signing up ${user.username}`);
 });
 
 module.exports = router;
