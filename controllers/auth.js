2a63bb3ff3e59417e4d58afc8045001cba0a9b09
Cesar Preza <cpreza93@yahoo.com>
2024-12-18 21:07:17 -0800

    adds password encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,7 @@ router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs');
 });
 
-//Validation
+//Sign-Up Validation
 router.post('/sign-up', async (req, res) => {
     // Checks to see if the username is available
     const userInDatabase = await User.findOne({ username: req.body.username });
@@ -23,5 +23,39 @@ router.post('/sign-up', async (req, res) => {
     const user = await User.create(req.body);
     res.send(`thanks for signing up ${user.username}`);
 });
+
+// Sign-In Validation
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs');
+});
+
+//logic to check if a user exists and the password is correct
+// router.post('/sign-in', async (req, res) => {
+//     try {
+//         const userInDatabase = await User.findOne({
+//             username: req.body.username,
+//         });
+//         if (!userInDatabase) {
+//             return res.send('Login failed. Please try again');
+//         }
+
+//         const validPassword = bcrypt.compareSync(
+//             req.body.password,
+//             userInDatabase.password
+//         );
+//         if (!validPassword) {
+//             return res.send('login failed. please try again');
+//         }
+//         req.session.user = {
+//             username: userInDatabase.username,
+//             id: userInDatabase._id,
+//         };
+
+//         res.redirect('/');
+//     } catch (error) {
+//         console.error(error.message);
+//         res.status(500).send('Internal server error');
+//     }
+// });
 
 module.exports = router;
