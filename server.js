26ade25f12d1a2a1d9143f5653b5ea1fcc64f0c8
Cesar Preza <cpreza93@yahoo.com>
2024-12-18 18:45:11 -0800

    feat: adds auth route

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const app = express();
 const path = require('path');
-
+const authController = require('./controllers/auth.js');
 
 // ==================
 // CONFIGURE MONGOOSE
 // ==================
 require('./configs/database');
 
-
 // ********************
 //    MIDDLEWARE
 // ********************
 
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use('/auth', authController);
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ***************************
 //      ROUTES (I.N.D.U.C.E)
@@ -45,4 +45,4 @@ app.use('/', require('./routes/book'));
 
 app.listen(process.env.PORT, () => {
     console.log(`🎧 Server is running on http://localhost:${process.env.PORT}`);
-})
+});
